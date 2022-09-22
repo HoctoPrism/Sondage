9c124d122a165665b50521dd5a870070ae81f7ef
HoctoPrism <nemexxix@gmail.com>
2022-09-22 11:43:43 +0200

    WIP - backend nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './components/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+/* synchronize doit être en false pour la production, en true cela peut entrainer une perte de donnée */
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ backend/src/database/config.ts
@@ -0,0 +1,23 @@
+{
+  "development": {
+    "username": "maxime",
+    "password": "octo&@16",
+    "database": "sondage",
+    "host": "localhost",
+    "dialect": "mysql",
+  },
+  "test": {
+    "username": "maxime",
+    "password": "octo&@16",
+    "database": "sondage",
+    "host": "localhost",
+    "dialect": "mysql",
+  },
+  "production": {
+    "username": "maxime",
+    "password": "octo&@16",
+    "database": "sondage",
+    "host": "localhost",
+    "dialect": "mysql",
+  }
+}
